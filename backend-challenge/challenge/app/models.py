5131e6c670457499b4ad9538cc5eb5a55cceb600
Nirupama K <nirupama.karandikar@gmail.com>
2019-03-25 10:40:41 +0000

    Add backend-challenge app folder to this repo

---
+++ backend-challenge/challenge/app/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class Project(models.Model):
+    """ Project model """
+
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now=True)
+    uid = models.UUIDField(default=uuid.uuid4, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Schedule(models.Model):
+    """ Schedule model """
+
+    name = models.CharField(max_length=100)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True,
+                                related_name="schedules")
+    status = models.CharField(max_length=50, default="New")
+    schedule_file = models.FileField(upload_to='schedule_files/', default=None)
+    deadline = models.DateTimeField(default=None, null=True)
+
+
+class Calendar(models.Model):
+    """ Calendar model """
+
+    name = models.CharField(max_length=100)
+    calendar_id = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    """ Task model """
+
+    name = models.CharField(max_length=200)
+    task_id = models.CharField(max_length=20, default="")
+    start = models.DateTimeField(default=None, null=True)
+    end = models.DateTimeField(default=None)
+    calendar = models.ForeignKey(
+        Calendar, on_delete=models.DO_NOTHING, default=None, null=True,
+        related_name="tasks")
---
+++ backend-challenge/challenge/app/schema.py
@@ -0,0 +1,28 @@
+import challenge.app.models as mod
+from graphene_django import DjangoObjectType
+
+
+# Write your unit test here using PyTest.
+
+class ProjectType(DjangoObjectType):
+    """ Django object type for returning Projects to the user """
+
+    class Meta:
+        """ Uses the Project model defined in models.py """
+        model = mod.Project
+
+
+class ScheduleType(DjangoObjectType):
+    """ Django object type for returning Schedules to the user """
+
+    class Meta:
+        """ Uses the Schedule model defined in models.py """
+        model = mod.Schedule
+
+
+class TaskType(DjangoObjectType):
+    """ Django object type for returning Tasks to the user """
+
+    class Meta:
+        """ Uses the Task model defined in models.py """
+        model = mod.Task
